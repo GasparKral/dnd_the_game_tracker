09bec5ce78a094ebe6f60b00affd44f52960562a
Gaspar <gaspargkral@gmail.com>
2026-02-21 12:58:19 +0100

    Update: fine styling for dashboard nav

--- dnd-desktop/src/ui/layouts/dashboard_layout.rs
+++ dnd-desktop/src/ui/layouts/dashboard_layout.rs
@@ -3,17 +3,19 @@ use dioxus::prelude::*;
 
 #[component]
 pub fn DashboardLayout() -> Element {
-    let selectedTab: Routes = use_route();
     rsx!(
         main{
             class:"min-h-screen minw-screen flex flex-col",
             nav{
-                class:"w-full flex gap-2 bg-stone-900 px-12 py-2 items-center sticky top-0 z-100",
+                class:"w-full flex gap-2 bg-stone-900 px-12 py-2 items-center fixed top-0 z-100",
                 Link{class:"text-lg text-center px-2",to:"/lore","Lore"}
                 Link{class:"text-lg text-center px-2",to:"/players","Jugadores"}
                 Link{class:"text-lg text-center px-2",to:"/map","Mapa"}
             }
-            Outlet::<Routes>{}
+            div{
+                class:"mt-12",
+                Outlet::<Routes>{}
+            }
         }
     )
 }
